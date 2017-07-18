96657fd9ac4208766698ff9aecb47d71562236b1
Willow <revolyw@yeah.net>
2017-07-18 10:50:31 +0800

    添加判断字词串长度及截断字词串长度的方法

--- java/utils/StringUtil.java
+++ java/utils/StringUtil.java
@@ -38,4 +38,67 @@ public class StringUtil {
         }
         return false;
     }
+
+    /**
+     * 计算字词串长度
+     * 英文单词算1个长度
+     *
+     * @param str
+     * @return
+     */
+    public static int lengthForWordAndChinese(String str) {
+        if (isEmpty(str)) {
+            return 0;
+        }
+        int length = 0;
+        str = str.trim();
+        String[] splitArray = str.split(" ");
+        for (String stringUnit : splitArray) {
+            if (isEnglishWord(stringUnit)) {
+                length++;
+            } else {
+                length += stringUnit.length();
+            }
+        }
+        return length;
+    }
+
+    /**
+     * 截断字词串
+     * 英文单词算1个词
+     *
+     * @param str
+     * @return
+     */
+    public static String substringForWordAndChinese(String str, int limit) {
+        if (isEmpty(str)) {
+            return "";
+        }
+        StringBuilder substring = new StringBuilder();
+        int length = 0;
+        str = str.trim();
+        String[] splitArray = str.split(" ");
+        for (String stringUnit : splitArray) {
+            if (isEnglishWord(stringUnit)) {
+                if (++length > limit) {
+                    //超过limit，返回
+                    break;
+                }
+                substring.append(stringUnit).append(" ");
+            } else {
+                int unitLength = stringUnit.length();
+                int redundantOffset = length + unitLength - limit;
+                if (redundantOffset > 0) {
+                    //超limit，处理完返回
+                    String appendString = stringUnit.substring(0, unitLength - redundantOffset);
+                    substring.append(appendString);
+//                    length += appendString.length();
+                    break;
+                }
+                length += unitLength;
+                substring.append(stringUnit).append(" ");
+            }
+        }
+        return substring.toString().trim();
+    }
 }
