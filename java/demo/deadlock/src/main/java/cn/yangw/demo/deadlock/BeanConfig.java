2cfc83201d066dc708cd79ef1463175964cd978c
yangw <revolyw@yeah.net>
2018-10-20 00:36:12 +0800

    :sparkles: add: TaskExecutor example

---
+++ java/demo/deadlock/src/main/java/cn/yangw/demo/deadlock/BeanConfig.java
@@ -0,0 +1,24 @@
+package cn.yangw.demo.deadlock;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class BeanConfig {
+    @Bean
+    public TaskExecutor threadPoolTaskExecutor(){
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        threadPoolTaskExecutor.setQueueCapacity(25);
+        return threadPoolTaskExecutor;
+    }
+
+    @Bean
+    public TaskExecutorExample executorExample(){
+        return new TaskExecutorExample(threadPoolTaskExecutor());
+    }
+
+}
--- java/demo/deadlock/src/main/java/cn/yangw/demo/deadlock/DeadlockApplication.java
+++ java/demo/deadlock/src/main/java/cn/yangw/demo/deadlock/DeadlockApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeadlockApplication {
+    private static TaskExecutorExample executorExample = null;
+
+    public DeadlockApplication(TaskExecutorExample executorExample) {
+        DeadlockApplication.executorExample = executorExample;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DeadlockApplication.class, args);
+        DeadlockApplication.executorExample.printMessages();
+        System.out.println("done");
     }
 }
