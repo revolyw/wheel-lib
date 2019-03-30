461eec6b10731fed8203923d8702aef28a5c92ed
Willow <revolyw@yeah.net>
2019-03-30 20:36:57 +0800

    rest doc demo

---
+++ java/demo/spring-rest-docs/src/main/java/cn/yangw/demo/spring/rest/docs/SpringRestDocsApplication.java
@@ -0,0 +1,12 @@
+package cn.yangw.demo.spring.rest.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDocsApplication.class, args);
+	}
+}
