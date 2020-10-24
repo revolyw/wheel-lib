25c37376972bf116ace07f9839d59450a5d9ac99
yangw <yangw@dxy.cn>
2020-10-24 14:42:40 +0800

    add vue hello world project

---
+++ front-end/hello-world/src/components/vModel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+      <p>{{ message }}</p>
+      <input v-model="message">
+  </div>
+</template>
+    
+<script>
+
+export default {
+  name: 'vModel',
+  el: '#app',
+  data: function(){
+    return { 
+      message: 'Runoob!'
+    }
+  }
+}
+</script>
