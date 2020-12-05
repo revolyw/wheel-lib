007b35f743675e1d8e698c5fb266f7400a64ed7a
Willow <yangw@dxy.cn>
2020-12-06 06:21:12 +0800

    add spring-mvc-analysis

---
+++ spring/spring-mvc-analysis/src/main/java/cn/willow/controller/GoController.java
@@ -0,0 +1,35 @@
+package cn.willow.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GoController {
+    private final Log logger = LogFactory.getLog(GoController.class);
+
+    @RequestMapping(value = {"/"}, method = RequestMethod.HEAD)
+    public String head() {
+        return "go.jsp";
+    }
+
+    /**
+     * 如果没有配置 {@link org.springframework.web.servlet.ViewResolver}
+     * Spring MVC 将默认使用 {@link org.springframework.web.servlet.view.InternalResourceViewResolver}
+     * 而且其 prefix 和 suffix 都为空
+     * 所以 go.jsp 返回值对应的就是根目录下的 go.jsp 文件
+     *
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = {"/index", "/"}, method = RequestMethod.GET)
+    public String index(Model model) throws Exception {
+        logger.info("processed by index");
+        model.addAttribute("msg", "GO GO GO!");
+        return "go.jsp";
+    }
+}
---
+++ spring/spring-mvc-analysis/web/go.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: willow
+  Date: 2020/12/5
+  Time: 9:13 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Let's Go</title>
+</head>
+<body>
+${msg}
+</body>
+</html>
