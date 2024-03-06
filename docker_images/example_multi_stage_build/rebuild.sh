36ccf9fd340af1b6af02b1f5276c8a573f8494f9
yangw <yangw@dxy.cn>
2024-03-06 10:16:06 +0800

    Add example multi stage build dockerfile demo

--- docker_images/example_multi_stage_build/rebuild.sh
+++ docker_images/example_multi_stage_build/rebuild.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # 定义镜像名称和容器名称
-IMAGE_NAME="rsyslog:v1"
-CONTAINER_NAME="rsyslog"
-
-set -e
+IMAGE_NAME="stage_first:v1"
+CONTAINER_NAME="stage_first"
 
 # 构建Docker镜像
 docker build -t $IMAGE_NAME .
@@ -16,11 +14,7 @@ if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
 fi
 
 # 运行新容器
-CONTAINER_ID=$(docker run --name $CONTAINER_NAME -p 5145:5145/udp -d $IMAGE_NAME)
-
-# 发送两条 syslog
-echo "this is an log" | nc -u -w 0 127.0.0.1 5145
-echo "this is an log" | nc -u -w 0 127.0.0.1 5145
+CONTAINER_ID=$(docker run --name $CONTAINER_NAME -d $IMAGE_NAME)
 
 # 进入容器
 docker exec -it $CONTAINER_ID /bin/bash
--- docker_images/example_multi_stage_build/rebuild_final.sh
+++ docker_images/example_multi_stage_build/rebuild_final.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 定义镜像名称和容器名称
-IMAGE_NAME="rsyslog:v1"
-CONTAINER_NAME="rsyslog"
+IMAGE_NAME="stage_final:v1"
+CONTAINER_NAME="stage_final"
 
+# 设置在命令返回非零状态时停止脚本执行
 set -e
 
 # 构建Docker镜像
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f DockerfileFinal --build-arg BASE_IMAGE=stage_first:v1 .
 
 # 检查并停止旧容器
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
@@ -16,11 +17,7 @@ if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
 fi
 
 # 运行新容器
-CONTAINER_ID=$(docker run --name $CONTAINER_NAME -p 5145:5145/udp -d $IMAGE_NAME)
-
-# 发送两条 syslog
-echo "this is an log" | nc -u -w 0 127.0.0.1 5145
-echo "this is an log" | nc -u -w 0 127.0.0.1 5145
+CONTAINER_ID=$(docker run --name $CONTAINER_NAME -d $IMAGE_NAME)
 
 # 进入容器
 docker exec -it $CONTAINER_ID /bin/bash
