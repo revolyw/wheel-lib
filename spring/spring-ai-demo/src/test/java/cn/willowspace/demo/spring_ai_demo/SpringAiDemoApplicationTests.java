5a364466292e77148f93b5b638841e24d9ba9131
yangw <yangw@dxy.cn>
2025-02-19 16:46:50 +0800

    feat:Spring AI + Ollama + Cursor 实现本地 AI 聊天机器人

---
+++ spring/spring-ai-demo/src/test/java/cn/willowspace/demo/spring_ai_demo/SpringAiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.willowspace.demo.spring_ai_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
